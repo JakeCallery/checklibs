02331a60d78910b4bbcd40851be2feb1ed6da3d0
Jake Callery <jake@subvoicestudios.com>
2018-06-13 10:08:14 -0400

    convert file contents in to a vector of strings, alternate method

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,14 @@ use std::env;
 use std::io::BufReader;
 use std::string::String;
 
+/*
 fn lines_from_file(file: &File) -> Vec<String> {
     let buff_reader = BufReader::new(file);
     buff_reader.lines()
        .map(|line| line.expect("Could not parse line"))
        .collect()
 }
+*/
 
 fn main() {
 
@@ -30,9 +32,23 @@ fn main() {
         Ok(file) => file,
     };
 
-    let lines: Vec<String> = lines_from_file(&file);
-    for line in lines {
-        println!("{:?}", line);
+    //let lines: Vec<String> = lines_from_file(&file);
+
+    let buf_reader: BufReader<&File> = BufReader::new(&file);
+    let lines_itr = buf_reader.lines();
+    let mut lines: Vec<String> = Vec::new();
+
+    for line in lines_itr {
+        //println!("{:?}", line);
+        match line {
+            Err(why) => panic!("Could not parse line: {}", why.description()),
+            Ok(_) =>
+                {
+                    let str:String = line.unwrap();
+                    println!("{:?}", str);
+                    lines.push(str);
+                },
+        }
     }
 
 }
