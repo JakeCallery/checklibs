80df1b6a32fdee4393c30bf3127804dc920724ea
Jake Callery <jake@subvoicestudios.com>
2018-06-13 09:15:10 -0400

    reads in file to a string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::error::Error;
+use std::fs::File;
+use std::path::Path;
+use std::path::Display;
+use std::path::PathBuf;
+use std::io::prelude::*;
+use std::env;
+
 fn main() {
-    println!("Hello");
+
+    let path: &Path = Path::new("./src/rpmoutput.txt");
+    let display: Display = path.display();
+
+    let cur_path: PathBuf = env::current_dir().unwrap();
+    let cur_path_disp: Display = cur_path.display();
+    println!("CWD: {}", cur_path_disp);
+
+    println!("Opening File");
+    let mut file: File = match File::open(&path) {
+        Err(why) => panic!("Couldn't read {}: {}", display, why.description()),
+        Ok(file) => file,
+    };
+
+    let mut str: String = String::new();
+    match file.read_to_string(&mut str) {
+        Err(why) => panic!("Could not read {}: {}", display, why.description()),
+        Ok(_) => print!("{} contains:{}\n", display, str),
+    }
+
+
 }
