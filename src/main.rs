14e14e71048faf751d69436863407d76cb8ccf44
Jake Callery <jcallery@subvoicestudios.com>
2018-06-17 16:26:31 -0400

    added argparse crate now reads in arguments

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ use std::env;
 use std::io::BufReader;
 use std::string::String;
 
+extern crate argparse;
+use argparse::{ArgumentParser, Store};
+
 fn lines_from_file(file: &File) -> Vec<String> {
     let buff_reader = BufReader::new(file);
     buff_reader.lines()
@@ -16,6 +19,27 @@ fn lines_from_file(file: &File) -> Vec<String> {
 }
 
 fn main() {
+
+    let args: Vec<String> = env::args().collect();
+    println!("ARGS: {:?}", args);
+
+    let mut cust_lib_file_path: String = "".to_string();
+    let mut req_lib_file_path: String = "".to_string();
+
+    {//argparse scope
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Compare a customer's rpm -qa output to list of requried libraries");
+        ap.refer(&mut cust_lib_file_path).add_option(&["-c", "--custlibs"], Store, "Path to customer library list from rpm -qa").required();
+        ap.refer(&mut req_lib_file_path).add_option(&["-r", "--reqlibs"], Store, "Path to required library list").required();
+        //ap.parse_args_or_exit();
+        match ap.parse_args() {
+            Ok(()) => {},
+            Err(err) => {
+                println!("Error: {:?}", err);
+                std::process::exit(err);
+            }
+        }
+    }
 
     let cust_libs_path: &Path = Path::new("./src/rpmoutput.txt");
     let cust_libs_display: Display = cust_libs_path.display();
@@ -40,11 +64,16 @@ fn main() {
     };
 
     println!("Saving Customer lines to memory");
+    #[allow(unused_variables)]
     let cust_lines_itr: Vec<String> = lines_from_file(&cust_libs_file);
 
     println!("Saving Required lines to memory");
+    #[allow(unused_variables)]
     let req_lines_itr: Vec<String> = lines_from_file(&req_libs_file);
 
+
+
+/*
     for line in cust_lines_itr {
         println!("{:?}", line);
     }
@@ -52,5 +81,5 @@ fn main() {
     for line in req_lines_itr {
         println!("{:?}", line);
     }
-
+*/
 }
